ceb5239ba42ad1ecb5810e8c2182c4db47689390
Omar <omar@notarize.com>
2021-09-24 08:42:37 -0700

    remove RxSwiftExt dependency

--- Package.swift
+++ Package.swift
@@ -15,20 +15,18 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift", .exact("6.2.0")),
-        .package(url: "https://github.com/RxSwiftCommunity/RxSwiftExt", .exact("6.1.0")),
         .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios", .exact("0.48.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "Entities",
-                      url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/Entities.xcframework-spm.zip",
-                      checksum: "fe4eb4acd9aa7db6b98849f2671caa21e9d8623fef840cdd76f8fd6fd04a4339"),
+                      url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/Entities.xcframework-spm-ext.zip",
+                      checksum: "65f3a3051f9241f35c0a76f4cdc41911c4b5c4c7f17664b8135bd53dcd8fbb5b"),
         .target(name: "NotarizeDeps", dependencies: [
             "RxSwift",
             .product(name: "RxCocoa", package: "RxSwift"),
             .product(name: "RxRelay", package: "RxSwift"),
-            "RxSwiftExt",
             "Apollo",
             .product(name: "ApolloAPI", package: "Apollo"),
             .product(name: "ApolloUtils", package: "Apollo")
